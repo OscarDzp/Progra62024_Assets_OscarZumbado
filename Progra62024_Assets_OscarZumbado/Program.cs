43ccbcbdb7c03c4735eb9d9d928ee73a0270df4d
Oscar <ozumbado2000@gmail.com>
2024-02-26 09:24:37 -0600

    semana 5

--- Progra62024_Assets_OscarZumbado/Program.cs
+++ Progra62024_Assets_OscarZumbado/Program.cs
@@ -1,23 +1,55 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Progra62024_Assets_OscarZumbado.Models;
 
-// Add services to the container.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        //Aca agregamos los valores de conexion a la base de datos para nuestra aplicacion
+
+        //1. agregar la info en una etiquta en el archivo appsettins.json
+
+        var CnnStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+
+        //por seguridad es mejor agregar la info de la contrasenia en este codigo o
+        //podriamos tener un almacen externo.
+        //Investigar que son los user secrets
+        CnnStrBuilder.Password = "progra6";
+
+        //ahora que hemos extraido la info de la cadena de conexion
+        //creamos una variable tipo string que la almacene
+        string CnnStr = CnnStrBuilder.ConnectionString;
+
+        //ahora conectamos el proyecto a la base de datos.
+
+        builder.Services.AddDbContext<Progra620241Context>(options => options.UseSqlServer(CnnStr));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
